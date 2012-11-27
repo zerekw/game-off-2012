c389e6da4a03f47c8b93c0027cddd0a685740287
Zerek Welz <zerek.welz@gmail.com>
2012-11-27 15:27:00 -0700

    Trying to get dijit working

--- src/app/ui/Desktop.js
+++ src/app/ui/Desktop.js
@@ -2,12 +2,13 @@ define([
 	"dojo/_base/declare",
 	"dijit/_WidgetBase",
 	"dijit/_TemplatedMixin",
-	"dojo/text!./templates/Desktop.html"
-], function(declare, _WidgetBase, _TemplatedMixin, template){
+	"dojo/text!./templates/Desktop.html",
+	"dijit/layout/BorderContainer",
+	"dijit/layout/ContentPane"
+], function(declare, _WidgetBase, _TemplatedMixin, template, BorderContainer, ContentPane){
 	return declare([_WidgetBase, _TemplatedMixin], {
 		baseClass: "app",
 		templateString: template,
-
 		socket: null
 	});
 });
